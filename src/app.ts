3b17a0a5d33fc40763850b30afc9ea46f7b16c9e
Rytis Mitkus <77438479+RytisMitkus@users.noreply.github.com>
2022-08-09 22:01:29 +0300

    Added prisma

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,34 @@
-import { config } from 'dotenv';
+import { config } from 'dotenv'
 import express, {
-  Application, Request, Response,
-} from 'express';
-import morgan from 'morgan';
+	Application, Request, Response
+} from 'express'
+import morgan from 'morgan'
 
-config();
+config()
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 app.use(express.json({
-  limit: '50mb',
-}));
+	limit: '50mb'
+}))
 app.use(express.urlencoded({
-  limit: '50mb',
-  extended: true,
-}));
+	limit: '50mb',
+	extended: true
+}))
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Babies backend server, hello stranger!');
-});
+	res.send('Babies backend server, hello stranger!')
+})
 
 app.get('/name', (req: Request, res: Response) => {
-  res.json({ myName: 'rytis' });
-});
+	res.json({
+		myName: 'rytis'
+	})
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port http://localhost:${PORT}`);
-});
+	console.log(`Server is listening on port http://localhost:${PORT}`)
+})
